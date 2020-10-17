347978f457d1b06a95b3c1460bfb018f836976d4
Admor Aguilar <admor.aguilar@gmail.com>
2020-10-17 16:39:34 +0800

    * Implement Midnight.Pages * First working VideoFeedSection

---
+++ Assets/HoloverseClient/Elements/Controllers/VideoFeedLoader.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Midnight;
+using Midnight.Pages;
+using Midnight.Concurrency;
+
+namespace Holoverse.Client
+{
+	using Api.Data.Common;
+	using Api.Data.Contents.Creators;
+	using Api.Data.Contents.Videos;
+
+	using Client.Pages;
+	using MongoDB.Bson;
+
+	public class VideoFeedLoader : MonoBehaviour
+	{
+		[SerializeField]
+		private HoloverseDataClientObject _client = null;
+
+		[SerializeField]
+		private Page _homePage = null;
+
+		private async Task InitializeFeedAsync()
+		{
+			List<Creator> creators = new List<Creator>();
+
+			using(new StopwatchScope("Getting creators data..", "Start", "End")) {
+				FindResults<Creator> resultCreators = await _client
+						.client.contents
+						.creators.FindCreatorsAsync(
+							new FindCreatorsSettings {
+								isCheckForAffiliations = true,
+								affiliations = new List<string> {
+									"hololiveJapan"
+								}
+							});
+
+				while(await resultCreators.MoveNextAsync()) {
+					creators.AddRange(resultCreators.current);
+				}
+			}
+
+			VideoFeedSection videoFeed = _homePage.GetSection<VideoFeedSection>();
+			VideoFeedSection.ContentInfo[] contentInfos = new VideoFeedSection.ContentInfo[] {
+				new VideoFeedSection.ContentInfo {
+					type = "Discover",
+					query = new FindCreatorVideosSettings<Video> {
+						creatorIdsUniversal = creators.Select(c => c.universalId).ToList(),
+						sortMode = FindCreatorVideosSettings<Video>.SortMode.ByCreationDate,
+						isSortAscending = false
+					}
+				},
+
+				new VideoFeedSection.ContentInfo {
+					type = "Community",
+					query = new FindCreatorRelatedVideosSettings<Video> {
+						creators = creators,
+						sortMode = FindVideosSettings<Video>.SortMode.ByCreationDate,
+						isSortAscending = false
+					}
+				},
+				new VideoFeedSection.ContentInfo {
+					type = "Broadcasts",
+					query = new FindCreatorVideosSettings<Video> {
+						isBroadcast = true,
+						creatorIdsUniversal = creators.Select(c => c.universalId).ToList(),
+						sortMode = FindCreatorVideosSettings<Video>.SortMode.ByCreationDate,
+						isSortAscending = false
+					}
+				},
+			};
+			videoFeed.Initialize(contentInfos);
+
+			foreach(VideoFeedSection.ContentInfo info in contentInfos) {
+				MLog.Log($"Type: {info.type} | Query: {info.query.GetFilterDocument().ToString()}");
+			}
+
+			//await _homePage.LoadAsync();
+		}
+
+		private void Start()
+		{
+			TaskExt.FireForget(InitializeFeedAsync());
+		}
+	}
+}
---
+++ Assets/HoloverseClient/Elements/Pages/Sections/VideoFeedSection.cs
@@ -0,0 +1,153 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using TMPro;
+using Midnight.Web;
+using Midnight.Pages;
+
+namespace Holoverse.Client.Pages
+{
+	using Api.Data.Common;
+	using Api.Data.Contents.Videos;
+
+	using Client.UI;
+	using Midnight;
+
+	public class VideoFeedSection : Section
+	{
+		public class ContentInfo
+		{
+			public string type;
+			public FindSettings<Video> query = null;
+		}
+
+		[Space]
+		[SerializeField]
+		private HoloverseDataClientObject _clientObject = null;
+
+		[SerializeField]
+		private VideoScrollRect _videoScroll = null;
+
+		public int amountPerLoad = 15;
+		public int cellRemainingToLoadMoreCount = 7;
+
+		[Space]
+		[SerializeField]
+		private TMP_Dropdown _dropdown = null;
+
+		private List<ContentInfo> _contentInfo = new List<ContentInfo>();
+		private List<VideoScrollRectCellData> _cellData = new List<VideoScrollRectCellData>();
+		private FindSettings<Video> _findSettings = null;
+		private FindResults<Video> _findResults = null;
+
+		private CancellationTokenSource _cts = new CancellationTokenSource();
+		private bool _isLoading = false;
+
+		public void Initialize(IEnumerable<ContentInfo> contentInfo)
+		{
+			Assert.IsNotNull(contentInfo);
+			Assert.IsTrue(contentInfo.Count() > 0);
+
+			Clear();
+
+			_contentInfo.AddRange(contentInfo);
+
+			_dropdown.ClearOptions();
+			_dropdown.AddOptions(contentInfo.Select(c => c.type).ToList());
+
+			SetContent(0);
+		}
+
+		public void SetContent(int index)
+		{
+			_findSettings = _contentInfo[index].query;
+		}
+
+		public void Clear()
+		{
+			_cellData.Clear();
+			_videoScroll.UpdateData(_cellData);
+		}
+
+		protected override async Task LoadContentAsync(CancellationToken cancellationToken = default)
+		{
+			_isLoading = true;
+
+			Clear();
+
+			_findResults = await _clientObject.client
+				.contents.videos
+				.FindVideosAsync(_findSettings, cancellationToken);
+
+			_videoScroll.OnScrollerPositionChanged += OnScrollerPositionChanged;
+			_dropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+
+			await LoadMoreContentAsync(cancellationToken);
+
+			_isLoading = false;
+		}
+
+		private async Task LoadMoreContentAsync(CancellationToken cancellationToken = default)
+		{
+			_isLoading = true;
+
+			if(!await _findResults.MoveNextAsync(cancellationToken)) {
+				MLog.Log(nameof(VideoFeedSection), $"No more videos found!");
+				return; 
+			}
+			foreach(Video video in _findResults.current) {
+				VideoScrollRectCellData cellData = new VideoScrollRectCellData {
+					thumbnail = await ImageGetWebRequest.GetAsync(video.thumbnailUrl),
+					title = video.title,
+					channel = video.creator,
+					onClick = () => Application.OpenURL(video.url)
+				};
+				_cellData.Add(cellData);
+			}
+
+			_videoScroll.UpdateData(_cellData);
+
+			_isLoading = false;
+		}
+
+		protected override async Task UnloadContentAsync()
+		{
+			await Task.CompletedTask;
+			Clear();
+
+			_videoScroll.OnScrollerPositionChanged -= OnScrollerPositionChanged;
+			_dropdown.onValueChanged.RemoveListener(OnDropdownValueChanged);
+		}
+
+		private CancellationTokenSource CreateCancelToken()
+		{
+			CancelToken();
+			return _cts = new CancellationTokenSource();
+		}
+
+		private void CancelToken()
+		{
+			if(_cts != null) {
+				_cts.Cancel();
+				_cts.Dispose();
+				_cts = null;
+			}
+		}
+
+		private async void OnScrollerPositionChanged(float position)
+		{
+			if(position >= _videoScroll.itemCount - cellRemainingToLoadMoreCount) {
+				if(!_isLoading) { await LoadMoreContentAsync(); }
+			}
+		}
+
+		private async void OnDropdownValueChanged(int value)
+		{
+			SetContent(value);
+			await RefreshAsync();
+		}
+	}
+}
