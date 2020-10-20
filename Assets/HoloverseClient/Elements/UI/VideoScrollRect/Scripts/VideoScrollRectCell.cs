d0d0ea91c8214d0fdcbc4c2cb7a94b07ba6a6f83
Admor Aguilar <admor.aguilar@gmail.com>
2020-10-21 01:27:31 +0800

    * Update VideoButton design for a more modern look

--- Assets/HoloverseClient/Elements/UI/VideoScrollRect/Scripts/VideoScrollRectCell.cs
+++ Assets/HoloverseClient/Elements/UI/VideoScrollRect/Scripts/VideoScrollRectCell.cs
@@ -2,7 +2,6 @@
 using UnityEngine.UI;
 using TMPro;
 using FancyScrollView;
-using Midnight;
 
 namespace Holoverse.Client.UI
 {
@@ -12,17 +11,17 @@ namespace Holoverse.Client.UI
 		private Image _thumbnailImage = null;
 
 		[SerializeField]
-		private TMP_Text _titleText = null;
+		private Image _creatorImage = null;
 
 		[SerializeField]
-		private TMP_Text _channelText = null;
+		private TMP_Text _creatorText = null;
+
+		[SerializeField]
+		private TMP_Text _titleText = null;
 
 		[Space]
 		[SerializeField]
 		private Button _button = null;
-
-		private Vector2 _baseTitleTextSize = Vector2.zero;
-		private Vector2 _baseChannelTextSize = Vector2.zero;
 
 		private void OnButtonClicked(VideoScrollRectCellData itemData)
 		{
@@ -33,17 +32,10 @@ namespace Holoverse.Client.UI
 		{
 			_thumbnailImage.sprite = itemData.thumbnail;
 			_titleText.text = itemData.title;
-			_channelText.text = itemData.channel;
+			_creatorText.text = itemData.channel;
 
 			_button.onClick.RemoveAllListeners();
 			_button.onClick.AddListener(() => OnButtonClicked(itemData));
-		}
-
-		private void Awake()
-		{
-			Canvas.ForceUpdateCanvases();
-			_baseTitleTextSize = _titleText.rectTransform.rect.size;
-			_baseChannelTextSize = _channelText.rectTransform.rect.size;
 		}
 	}
 }
