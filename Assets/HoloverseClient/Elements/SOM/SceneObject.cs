eddf21f5bcf900fb9de7f5653b04e27328c8c25b
Admor Aguilar <admor.aguilar@gmail.com>
2020-10-24 00:07:20 +0800

    * SceneObjectModel WIP

--- Assets/HoloverseClient/Elements/SOM/SceneObject.cs
+++ Assets/HoloverseClient/Elements/SOM/SceneObject.cs
@@ -2,5 +2,23 @@
 
 namespace Midnight.SOM
 {
-	public class SceneObject : MonoBehaviour { }
+	[DefaultExecutionOrder(-1)]
+	public class SceneObject : MonoBehaviour 
+	{
+		private SceneObjectModel _model = null;
+
+		public new Transform transform => this.GetComponent(ref _transform, () => base.transform);
+		private Transform _transform = null;
+
+		private void Awake()
+		{
+			_model = SceneObjectModel.Get(this);
+			_model.Add(this);
+		}
+
+		private void OnDestroy()
+		{
+			_model.Remove(this);
+		}
+	}
 }
--- Assets/HoloverseClient/Elements/SOM/SceneObjectModel.cs
+++ Assets/HoloverseClient/Elements/SOM/SceneObjectModel.cs
@@ -4,11 +4,61 @@ using UnityEngine;
 
 namespace Midnight.SOM
 {
-	[DefaultExecutionOrder(-1)]
+	[DefaultExecutionOrder(-2)]
 	public class SceneObjectModel : MonoBehaviour
 	{
-		private Dictionary<Type, Component> _cache = new Dictionary<Type, Component>();
+		private static Dictionary<int, List<SceneObjectModel>> _sceneObjectModels = new Dictionary<int, List<SceneObjectModel>>();
+
+		public static SceneObjectModel Get(int sceneHandle)
+		{
+			SceneObjectModel result = null;
+			
+			List<SceneObjectModel> models = GetModelList(sceneHandle);
+			if(models.Count > 0) { result = models[0]; }
+
+			return result;
+		}
+
+		public static SceneObjectModel Get(SceneObject sceneObject)
+		{
+			SceneObjectModel result = null;
+
+			List<SceneObjectModel> models = GetModelList(sceneObject.GetInstanceID());
+			foreach(SceneObjectModel model in models) {
+				if(sceneObject.transform.IsChildOf(model.transform)) {
+					result = model;
+					break;
+				}
+			}
+
+			return result;
+		}
+
+		private static void Add(SceneObjectModel model)
+		{
+			List<SceneObjectModel> models = GetModelList(model.gameObject.scene.handle);
+			models.Add(model);
+		}
+
+		private static void Remove(SceneObjectModel model)
+		{
+			List<SceneObjectModel> models = GetModelList(model.gameObject.scene.handle);
+			models.Remove(model);
+		}
+
+		private static List<SceneObjectModel> GetModelList(int sceneHandle)
+		{
+			if(!_sceneObjectModels.TryGetValue(sceneHandle, out List<SceneObjectModel> models)) {
+				_sceneObjectModels[sceneHandle] = models = new List<SceneObjectModel>();
+			}
+			return models;
+		}
+
+		private Dictionary<Type, List<Component>> _componentCacheLookup = new Dictionary<Type, List<Component>>();
 		private List<SceneObject> _sceneObjects = new List<SceneObject>();
+
+		public new Transform transform => this.GetComponent(ref _transform, () => base.transform);
+		private Transform _transform = null;
 
 		public T GetSceneObjectComponent<T>() where T : Component
 		{
@@ -19,41 +69,72 @@ namespace Midnight.SOM
 		{
 			Component result = null;
 
-			if(_cache.TryGetValue(type, out result)) {
-				return result;
-			}
-
-			foreach(SceneObject sceneObject in _sceneObjects) {
-				if(sceneObject.TryGetComponent(type, out result)) {
-					_cache[type] = result;
-					break;
+			if(_componentCacheLookup.TryGetValue(type, out List<Component> cache)) {
+				result = cache[0];
+			} else {
+				foreach(SceneObject sceneObject in _sceneObjects) {
+					if(sceneObject.TryGetComponent(type, out Component component)) {
+						AddComponentsToCache(component);
+						result = component;
+					}
 				}
 			}
 
 			return result;
 		}
 
-		private void RefreshSceneObjects()
+		public void Add(SceneObject sceneObject)
 		{
-			_cache.Clear();
+			AddComponentsToCache(sceneObject.GetComponents<Component>());
+		}
 
-			_sceneObjects.Clear();
-			GetComponentsInChildren(true, _sceneObjects);
+		public void Add(Component component)
+		{
+			AddComponentsToCache(component);
+		} 
+
+		public void Remove(SceneObject sceneObject)
+		{
+			RemoveComponentsFromCache(sceneObject.GetComponents<Component>());
+		}
+
+		public void Remove(Component component)
+		{
+			RemoveComponentsFromCache(component);
+		}
+
+		private void AddComponentsToCache(params Component[] components)
+		{
+			foreach(Component component in components) {
+				Type[] types = component.GetType().GetAllTypeAndInterfaceBases(typeof(MonoBehaviour), true, false);
+				foreach(Type type in types) { GetOrCreateComponentCache(type).Add(component); }
+			}
+		}
+
+		private void RemoveComponentsFromCache(params Component[] components)
+		{
+			foreach(Component component in components) {
+				Type[] types = component.GetType().GetAllTypeAndInterfaceBases(typeof(MonoBehaviour), true, false);
+				foreach(Type type in types) { GetOrCreateComponentCache(type).Remove(component); }
+			}
+		}
+
+		private List<Component> GetOrCreateComponentCache(Type type)
+		{
+			if(!_componentCacheLookup.TryGetValue(type, out List<Component> cache)) {
+				_componentCacheLookup[type] = cache = new List<Component>();
+			}
+			return cache;
 		}
 
 		private void Awake()
 		{
-			RefreshSceneObjects();
+			Add(this);
 		}
 
-		private void OnTransformParentChanged()
+		private void OnDestroy()
 		{
-			RefreshSceneObjects();
-		}
-
-		private void OnTransformChildrenChanged()
-		{
-			RefreshSceneObjects();
+			Remove(this);
 		}
 	}
 }
