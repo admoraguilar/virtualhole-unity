a571c5c22bf061b8069b250ddc979acdd7303b8c
Admor Aguilar <admor.aguilar@gmail.com>
2020-10-24 03:40:53 +0800

    * Initial Data Caching

---
+++ Assets/HoloverseClient/Elements/DataCache/DataCache.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Holoverse.Client.Caching
+{
+	using Api.Data.Contents.Creators;
+	using Api.Data.Contents.Videos;
+
+	public static partial class DataCache
+	{
+		public static KeyValueCache<string, Creator> creators { get; private set; } = null;
+		public static KeyValueCache<string, Video> videos { get; private set; } = null;
+
+		static DataCache()
+		{
+			creators = new KeyValueCache<string, Creator>((Creator c) => c.universalId);
+			videos = new KeyValueCache<string, Video>((Video v) => v.id);
+		}
+	}
+}
