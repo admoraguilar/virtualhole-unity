a26168329259c07372d615f0d97ed799079416ff
Admor Aguilar <admor.aguilar@gmail.com>
2020-10-28 01:21:12 +0800

    * Create Queries and Caches for Client.Data

---
+++ Assets/HoloverseClient/Elements/Data/Scripts/Cache/CreatorCache.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using Midnight.Web;
+
+namespace Holoverse.Client.Data
+{
+	using Api.Data.Contents.Creators;
+
+	public static class CreatorCache
+	{
+		private static Dictionary<string, Creator> _creatorLookup = new Dictionary<string, Creator>();
+		private static Dictionary<string, Sprite> _avatarLookup = new Dictionary<string, Sprite>();
+
+		public static async Task<Sprite> GetAvatarAsync(
+			string universalId, CancellationToken cancellationToken = default)
+		{
+			if(!_avatarLookup.TryGetValue(universalId, out Sprite avatar)) {
+				if(_creatorLookup.TryGetValue(universalId, out Creator creator)) {
+					_avatarLookup[universalId] = 
+						avatar = await ImageGetWebRequest.GetAsync(
+							creator.avatarUrl, null, 
+							cancellationToken);
+				}
+			}
+
+			return avatar;
+		}
+
+		public static Creator Get(string universalId)
+		{
+			_creatorLookup.TryGetValue(universalId, out Creator creator);
+			return creator;
+		}
+
+		public static void Add(IEnumerable<Creator> creators)
+		{
+			foreach(Creator creator in creators) { Add(creator); }
+		}
+
+		public static void Add(Creator creator)
+		{
+			_creatorLookup[creator.universalId] = creator;
+		}
+	}
+}
---
+++ Assets/HoloverseClient/Elements/Data/Scripts/Cache/VideoCache.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using Midnight.Web;
+
+namespace Holoverse.Client.Data
+{
+	using Api.Data.Contents;
+	using Api.Data.Contents.Videos;
+	
+
+	public static class VideoCache
+	{
+		private static Dictionary<string, Video> _videoLookup = new Dictionary<string, Video>();
+		private static Dictionary<string, Sprite> _thumbnailLookup = new Dictionary<string, Sprite>();
+
+		public static async Task<Sprite> GetThumbnailAsync(
+			Platform platform, string id, CancellationToken cancellationToken = default)
+		{
+			string key = CreateLookupKey(platform, id);
+			if(!_thumbnailLookup.TryGetValue(key, out Sprite thumbnail)) {
+				if(_videoLookup.TryGetValue(key, out Video video)) {
+					_thumbnailLookup[key] =
+						thumbnail = await ImageGetWebRequest.GetAsync(
+							video.thumbnailUrl, null,
+							cancellationToken);
+				}
+			}
+
+			return thumbnail;
+		}
+
+		public static Video Get(Platform platform, string id)
+		{
+			_videoLookup.TryGetValue(CreateLookupKey(platform, id), out Video video);
+			return video;
+		}
+
+		public static void Add(IEnumerable<Video> videos)
+		{
+			foreach(Video video in videos) { Add(video); }
+		}
+
+		public static void Add(Video video)
+		{
+			_videoLookup[CreateLookupKey(video.platform, video.id)] = video;
+		}
+
+		private static string CreateLookupKey(Platform platform, string id) => $"{platform}-{id}";
+	}
+}
