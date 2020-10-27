6ab87775eca161f7ee7801b7d998597054480b48
Admor Aguilar <admor.aguilar@gmail.com>
2020-10-27 23:14:20 +0800

    * Create CreatorQuery

---
+++ Assets/HoloverseClient/Elements/Data/Scripts/CreatorQuery.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Midnight;
+
+namespace Holoverse.Client.Data
+{
+	using Api.Data;
+	using Api.Data.Common;
+	using Api.Data.Contents.Creators;
+
+	public class CreatorQuery
+	{
+		public IReadOnlyDictionary<string, Creator> creatorLookup => _creatorLookup;
+		private Dictionary<string, Creator> _creatorLookup = new Dictionary<string, Creator>();
+
+		private HoloverseDataClient _client = null;
+		private FindCreatorsSettings _findCreatorsSettings = null;
+		private bool _isLoaded = false;
+
+		public CreatorQuery(HoloverseDataClient client, FindCreatorsSettings findCreatorsSettings)
+		{
+			_client = client;
+			_findCreatorsSettings = findCreatorsSettings;
+		}
+
+		public async Task LoadAsync(CancellationToken cancellationToken = default)
+		{
+			if(_isLoaded) { return; }
+
+			using(new StopwatchScope(nameof(VideoFeedQuery), "Start Getting creators data...", "End")) {
+				CreatorClient creatorClient = _client.contents.creators;
+				using(FindResults<Creator> results = await creatorClient.FindCreatorsAsync(_findCreatorsSettings, cancellationToken)) {
+					while(await results.MoveNextAsync()) {
+						AddCreatorsToLookup(results.current);
+					}
+				}
+			}
+
+			_isLoaded = true;
+		}
+
+		private void AddCreatorsToLookup(IEnumerable<Creator> creators)
+		{
+			foreach(Creator result in creators) {
+				_creatorLookup[result.universalId] = result;
+			}
+		}
+	}
+}
