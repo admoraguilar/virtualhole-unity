e1bec1734fb15bc4618fb9e93eab4d2b4198debe
Admor Aguilar <admor.aguilar@gmail.com>
2020-11-08 19:04:33 +0800

    * Optimization: Disable 32-bit display buffer * Optimization: Implement percentage-based resolution scaling * Disable rendering past safe area

--- Assets/VirtualHoleClient/Produce/Main/Scripts/Controllers/ResolutionController.cs
+++ Assets/VirtualHoleClient/Produce/Main/Scripts/Controllers/ResolutionController.cs
@@ -4,18 +4,33 @@ namespace VirtualHole.Client.Controllers
 {
 	public class ResolutionController : MonoBehaviour
 	{
+		[Range(0.3f, 1f)]
 		[SerializeField]
-		private int width = 720;
+		private float _resolutionScaling = 0f;
 
 		[SerializeField]
-		private int height = 1280;
+		private int _preferredRefreshRate = 60;
 
-		[SerializeField]
-		private int refreshRate = 60;
+		private Vector2Int _renderResolution = Vector2Int.zero;
+		private Vector2Int _deviceResolution = Vector2Int.zero;
+
+		private void UpdateRenderResolution()
+		{
+			float scale = Mathf.Max(0.3f, _resolutionScaling);
+			Vector2 desiredResolution = new Vector2(_deviceResolution.x * scale, _deviceResolution.y * scale);
+			_renderResolution = new Vector2Int((int)desiredResolution.x, (int)desiredResolution.y);
+			Screen.SetResolution(_renderResolution.x, _renderResolution.y, true, _preferredRefreshRate);
+		}
+
+		private void UpdateDeviceResolution()
+		{
+			_deviceResolution = new Vector2Int(Screen.width, Screen.height);
+		}
 
 		private void Start()
 		{
-			Screen.SetResolution(width, height, true, refreshRate);
+			UpdateDeviceResolution();
+			UpdateRenderResolution();
 		}
 	}
 }
