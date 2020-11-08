1ae9639cfd6096963c292cc9f1796f452aef3f4b
Admor Aguilar <admor.aguilar@gmail.com>
2020-11-08 18:37:00 +0800

    * Ensure TargetFramerate is always set when the app is active, and set it to 1 if idle * Remove pixel lighting count on Quality Settings for Non-Game

--- Assets/VirtualHoleClient/Produce/Main/Scripts/Controllers/FPSController.cs
+++ Assets/VirtualHoleClient/Produce/Main/Scripts/Controllers/FPSController.cs
@@ -7,9 +7,25 @@ namespace VirtualHole.Client.Controllers
 		[SerializeField]
 		private int _targetFramerate = 60;
 
-		void Start()
+		private void SetTargetFramerateActive(bool active)
 		{
-			Application.targetFrameRate = _targetFramerate;
+			if(!active) { Application.targetFrameRate = 1; } 
+			else { Application.targetFrameRate = _targetFramerate; }
+		}
+
+		private void Start()
+		{
+			SetTargetFramerateActive(true);
+		}
+
+		private void OnApplicationPause(bool pause)
+		{
+			SetTargetFramerateActive(pause);
+		}
+
+		private void OnApplicationFocus(bool focus)
+		{
+			SetTargetFramerateActive(focus);
 		}
 	}
 }
