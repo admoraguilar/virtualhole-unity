023acd4c9b22a007bfb9c50db86cae7c8bebbb8c
Admor Aguilar <admor.aguilar@gmail.com>
2020-11-19 01:30:50 +0800

    * Improve ScrollAntimistap by using CanvasGroup instead of an image

--- Assets/VirtualHoleClient/Elements/UI/Common/Scripts/ScrollComponents/ScrollAntiMistap.cs
+++ Assets/VirtualHoleClient/Elements/UI/Common/Scripts/ScrollComponents/ScrollAntiMistap.cs
@@ -1,16 +1,23 @@
 ﻿
 namespace UnityEngine.UI
 {
+	[RequireComponent(typeof(CanvasGroup))]
 	public class ScrollAntiMistap : MonoBehaviour
 	{
-		[SerializeField]
-		private GameObject _antiMistapImage = null;
-
 		[SerializeField]
 		private float _antiMistapTime = .1f;
 		private float _antiMistapTimer = 0f;
 
-		protected IScrollRect scrollRect
+		public CanvasGroup canvasGroup
+		{
+			get {
+				if(_canvasGroup == null) { _canvasGroup = GetComponent<CanvasGroup>(); }
+				return _canvasGroup;
+			}
+		}
+		private CanvasGroup _canvasGroup = null;
+
+		public IScrollRect scrollRect
 		{
 			get {
 				if(_scrollRect == null) { _scrollRect = GetComponent<IScrollRect>(); }
@@ -23,11 +30,11 @@ namespace UnityEngine.UI
 		{
 			if(Mathf.Abs(scrollRect.velocity.x) > 0.1f || 
 			   Mathf.Abs(scrollRect.velocity.y) > 0.1f) {
-				_antiMistapImage?.SetActive(true);
+				canvasGroup.interactable = false;
 				_antiMistapTimer = 0f;
 			} else {
 				if(_antiMistapTimer > _antiMistapTime) {
-					_antiMistapImage?.SetActive(false);
+					canvasGroup.interactable = true;
 				} else {
 					_antiMistapTimer += Time.fixedDeltaTime;
 				}
